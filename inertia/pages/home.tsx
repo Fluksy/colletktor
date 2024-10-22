a003e58ebf4de72b000adc7848ac11f6a7912b78
Fluksy <sellier.thomas@hotmail.fr>
2024-10-22 22:47:09 +0200

    Add login and signup pages

--- inertia/pages/home.tsx
+++ inertia/pages/home.tsx
@@ -1,9 +1,13 @@
-import { LoginForm } from './login'
+import { useState } from 'react'
+import { Button } from '~/lib/components/ui/button'
 
 export default function Home() {
+  const [counter, setCounter] = useState(0)
   return (
-    <>
-      <LoginForm />
-    </>
+    <div>
+      <p>hello from home</p>
+
+      <Button onClick={() => setCounter((counter) => counter + 1)}>Counter: {counter}</Button>
+    </div>
   )
 }
